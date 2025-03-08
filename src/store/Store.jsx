66889dde6ec62181e74a84821c6ba257fbfd8353
Priyanshu <pr129858@gmail.com>
2025-03-08 16:42:22 +0530

    DexMov Done

--- src/store/Store.jsx
+++ src/store/Store.jsx
@@ -1,7 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit'
 import  movieReducer  from './reducers/MovieSlice'
 import  PersonReducer  from './reducers/PersonSlice'
-import TvReducer  from './reducers/TvSlice'
+import TvReducer  from './reducers/Tvslice'
 
 export const store = configureStore({
   reducer: {
