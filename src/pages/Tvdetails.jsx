ebc0531c101dca1d95f57d4b8b4127ce3084ab77
Priyanshu <pr129858@gmail.com>
2025-03-08 18:02:47 +0530

    Tv Slice Added

--- src/pages/Tvdetails.jsx
+++ src/pages/Tvdetails.jsx
@@ -1,9 +1,9 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Link, Outlet, useLocation, useNavigate, useParams } from "react-router-dom";
-import { asyncLoadTv, removeTv } from "../store/actions/TvActions";
 import Loading from "../components/Loading";
 import HorizontalCards from "../components/partial/HorizontalCards";
+import { asyncLoadTv, removeTv } from "../Store/actions/TvActions.jsx";
 
 const TvDetails = () => {
   const { pathname } = useLocation();
