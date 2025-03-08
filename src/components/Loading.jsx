d3a1037037d825015024eb909fc2c287001a2b3c
Priyanshu <pr129858@gmail.com>
2025-03-08 16:45:45 +0530

    DexMov Done

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
-import Loader from '../../public/intersection.gif'
+// import Loader from '../../public/intersection.gif'
  const Loading = () => {
   return (
     <div className='w-screen h-screen flex justify-center items-center bg-black'>
-      <img className=' object-cover' src= {Loader}/>
+      {/* <img className='object-cover' src= {Loader}/> */}
+      <h1 className="text-3xl text-white font-semibold">Loading...</h1>
     </div>
   )
 }
