00ad2e98a70d6c413d99f09ad2472a703d375d60
Priyanshu <pr129858@gmail.com>
2025-03-08 17:01:26 +0530

    DexMov Done

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,13 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./components/Home";
 import Trending from "./components/Trending";
 import Popular from "./components/Popular";
-import Movie from "./components/Movie";
-import Webseries from "./components/Webseries";
-import People from "./components/People";
-import Moviedetails from "./components/Moviedetails";
-import Persondetails from "./components/Persondetails";
-import Tvdetails from "./components/Tvdetails";
+import Movie from "./pages/Movie";
+import Moviedetails from "./pages/Moviedetails";
+import Webseries from "./pages/Webseries";
+import TvDetails from "./pages/Tvdetails";
 import Trailer from "./components/partial/Trailer";
+import Persondetails from "./pages/Persondetails";
+import People from "./components/People";
 
 
 const App = () => {
@@ -24,11 +24,11 @@ const App = () => {
           <Route path="/movie/details/:id/trailer" element={<Trailer/>} />
         </Route>
         <Route path="/webseries" element={<Webseries />}/>
-        <Route path="/tv/details/:id" element={<Tvdetails />} >
+        <Route path="/tv/details/:id" element={<TvDetails />} >
         <Route path="/tv/details/:id/trailer" element={<Trailer/>} />
 
         </Route>
-        <Route path="/people" element={<People />}/>
+        <Route path="/people" element={<People/>}/>
         <Route path="/people/details/:id" element={<Persondetails />} />
       </Routes>
     </div>
